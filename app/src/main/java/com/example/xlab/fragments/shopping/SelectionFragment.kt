e95fc8be1e6b493f96c5c368fd3d153959ff8177
CarryBro <62814124+CarryBro@users.noreply.github.com>
2024-03-02 08:22:09 +0300

    28.02.2024

--- app/src/main/java/com/example/xlab/fragments/shopping/SelectionFragment.kt
+++ app/src/main/java/com/example/xlab/fragments/shopping/SelectionFragment.kt
@@ -1,7 +1,13 @@
 package com.example.xlab.fragments.shopping
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.xlab.R
 
-class SelectionFragment: Fragment(R.layout.fragment_selection) {
-}
+class SelectionFragment: Fragment(R.layout.sendmine) {
+
+
+        }
